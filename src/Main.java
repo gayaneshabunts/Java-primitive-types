38af1116af90e70c13077be4aa17152c5291c9f4
gayaneshabunts <gsabunc@gmail.com>
2022-12-26 00:29:39 +0000

    Primitive types

---
+++ src/Main.java
@@ -0,0 +1,24 @@
+public class Main {
+    public static void main(String[] args) {
+        // {type} varName=value
+        int a;
+        a = 5;
+        int b;
+        b = 8;
+        int c;
+        c = (a * a + b * b);
+        System.out.println("c=" + c);
+        // mas 2
+        c = 6;
+
+        int S;
+        S = (((a + b) / 2) * c);
+        System.out.println("S=" + S);
+        //mas 3
+        int d = 123;
+        System.out.println(123 / 100);
+        System.out.println((123 / 10) % 10);
+        System.out.println(123 % 10);
+
+    }
+}
